6b8d51f25f7979976dc31e7d3f2421ec177b667c
Alwin Joseph <alwinjosephkaduppil@gmail.com>
2022-06-30 09:15:29 +0530

    fixing the configs and UI improvements

--- main/urls.py
+++ main/urls.py
@@ -17,6 +17,8 @@ urlpatterns = [
     path('admin/products/', views.products_admin, name='admin_products'),
     path('admin/live_products/', views.products_live_admin, name='admin_live_products'),
     path('admin/deleted_products/', views.products_deleted_admin, name='admin_deleted_products'),
+    path('seller/', views.g_seller, name='seller'),
+    path('seller/submitted', views.productAdded, name='productAdded'),
     path('admin/products_details/<int:id>', views.product_details_admin),
     path('admin/live_products_details/<int:id>', views.product_live_details_admin),
     path('admin/deleted_products_details/<int:id>', views.deleted_product_details_admin),
--- main/views.py
+++ main/views.py
@@ -273,3 +273,95 @@ def deleted_product_details_admin(request, id):
 
     return render(request, 'deleted_products_details_admin copy.html',
                   {'product': product, 'seller_details': seller_details, 'BASE_DIR': BASE_DIR})
+
+
+@login_required(login_url='/login')
+def productAdded(request):
+    # request.session["login_status"]=False
+    # login_status = request.session["login_status"]
+    # if login_status:
+    return render(request,'product_added.html')
+    # else:
+    #     return redirect('/products/')
+
+@login_required(login_url='/login')
+def g_seller(request):
+    # login_status = request.session["login_status"]
+    # print(request.session["user_name"])
+    # print(request.session["user_email"])
+
+    # if login_status == True:
+    #     # print(request.session["user_email"])
+    if request.method == 'POST':
+        user_id = request.session["user_email"]
+        productName = request.POST.get("product_name", False)
+        productCategory = request.POST.get("product_category", False)
+        productPrice = request.POST.get("product_price", False)
+        productDescription = request.POST.get("product_description", False)
+        productImage1 = request.FILES.get("pro-image1", False)
+        productImage2 = request.FILES.get("pro-image2", False)
+        productImage3 = request.FILES.get("pro-image3", False)
+        productImage4 = request.FILES.get("pro-image4", False)
+        # print(productName)
+        # print(productImage1)
+        # print(productImage2)
+        # print(productImage3)
+        # print(productImage4)
+
+        product_Table = ProductsTable()
+        product_Table.user_id = user_id
+        product_Table.product_name = productName
+        product_Table.product_description = productDescription
+        product_Table.expected_price = productPrice
+        product_Table.category = productCategory
+        product_Table.product_image1 = productImage1
+        product_Table.product_image2 = productImage2
+        product_Table.product_image3 = productImage3
+        product_Table.product_image4 = productImage4
+
+        product_Table.save()
+
+        return redirect('/g/seller/submitted')
+
+    return render(request,'seller_page.html')
+
+@login_required(login_url='/login')
+def g_app_seller(request):
+    # login_status = request.session["login_status"]
+    # print(request.session["user_name"])
+    # print(request.session["user_email"])
+
+    # if login_status == True:
+    #     # print(request.session["user_email"])
+    if request.method == 'POST':
+        user_id = request.session["user_email"]
+        productName = request.POST.get("product_name", False)
+        productCategory = request.POST.get("product_category", False)
+        productPrice = request.POST.get("product_price", False)
+        productDescription = request.POST.get("product_description", False)
+        productImage1 = request.FILES.get("pro-image1", False)
+        productImage2 = request.FILES.get("pro-image2", False)
+        productImage3 = request.FILES.get("pro-image3", False)
+        productImage4 = request.FILES.get("pro-image4", False)
+        # print(productName)
+        # print(productImage1)
+        # print(productImage2)
+        # print(productImage3)
+        # print(productImage4)
+
+        product_Table = ProductsTable()
+        product_Table.user_id = user_id
+        product_Table.product_name = productName
+        product_Table.product_description = productDescription
+        product_Table.expected_price = productPrice
+        product_Table.category = productCategory
+        product_Table.product_image1 = productImage1
+        product_Table.product_image2 = productImage2
+        product_Table.product_image3 = productImage3
+        product_Table.product_image4 = productImage4
+
+        product_Table.save()
+
+        return redirect('/g/seller/submitted')
+
+    return render(request,'seller_page.html')
